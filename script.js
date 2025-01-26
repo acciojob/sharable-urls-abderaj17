ab6cc791f71b523b316a41790add952530417eb1
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-26 15:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const nameField = document.querySelector('#name')
+const yearField = document.querySelector('#year')
+const btn = document.querySelector('button');
+
+const headingUrl = document.querySelector('#url');
+
+btn.addEventListener('click', ()=>{
+	const name = nameField.value.trim();
+	const year = yearField.value;
+
+	headingUrl.textContent = `https://localhost:8080/?name=${name}&year=${year}`
+})
